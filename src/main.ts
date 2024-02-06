a4912caf7e8dac522b775cd3b18a4c4d549e7b25
lamvbg <lamvbgcd191320@fpt.edu.vn>
2024-02-06 20:15:30 +0700

    Enable Cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   
   // Cấu hình CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://ebayorder.netlify.app'],
+    origin: ['http://localhost:5173', 'https://ebayorder.netlify.app', 'https://ebay-store.onrender.com'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
     credentials: true,
