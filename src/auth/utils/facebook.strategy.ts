8715ac90523799190669d0e47306c3579b31a30f
lamvbg <lamvbgcd191320@fpt.edu.vn>
2024-04-08 15:36:30 +0700

    fixFbLogin

--- src/auth/utils/facebook.strategy.ts
+++ src/auth/utils/facebook.strategy.ts
@@ -15,9 +15,10 @@ export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
     super({
       clientID: '385743550645089',
       clientSecret: 'db5af3ac4c7b520cae6ef9051190e637',
-      callbackURL: isProduction
-        ? 'https://api-ebay.onrender.com/api/auth/facebook/redirect'
-        : 'http://localhost:2001/api/auth/facebook/redirect',
+      callbackURL: 'https://api-ebay.onrender.com/api/auth/facebook/redirect',
+      // isProduction
+      //   ? 'https://api-ebay.onrender.com/api/auth/facebook/redirect'
+      //   : 'http://localhost:2001/api/auth/facebook/redirect',
       scope: ['email', 'public_profile'],
       profileFields: ['id', 'email', 'displayName'],
     });
