2daf884bd7ea751af36559401905c44c54930840
lamvbg <lamvbgcd191320@fpt.edu.vn>
2024-05-04 23:35:12 +0700

    aaaa

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
   // const document = SwaggerModule.createDocument(app, config);
   // SwaggerModule.setup('api', app, document);
 
-  await app.listen(2001);
+  await app.listen(process.env.PORT || 2001);
 }
 bootstrap();
